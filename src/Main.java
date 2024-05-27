181644a5741daa0cf6ef9820ea72272207a7ddb5
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-05-27 15:47:53 +0200

    es 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+        System.out.println("Per favore inserire un numero:");
+        try {
+            String input = scanner.nextLine();
+            numero = Integer.parseInt(input);
+        } catch (NumberFormatException ex) {
+            System.err.println("Il valore che hai inserito non è un numero");
+        } finally {
+            scanner.close();
+        }
+
     }
 }
---
+++ src/es1/Esercizio1.java
@@ -0,0 +1,37 @@
+package es1;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Esercizio1 {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+        for (int i = 0; i < array.length; i++) {
+            Random rdnm = new Random();
+            int number = rdnm.nextInt(1, 10);
+            array[i] = number;
+            System.out.println(array[i]);
+        }
+        Scanner scanner = new Scanner(System.in);
+        try {
+            while (true) {
+                System.out.println("Dammi un numero da 1 a 5 per sostituire il valore corrispondente nell'array. Se non vuoi cambiare nulla premi 0");
+                int numeroDaSostituire = Integer.parseInt(scanner.nextLine());
+                array[numeroDaSostituire - 1] = numeroDaSostituire;
+                System.out.println(Arrays.toString(array));
+                if (numeroDaSostituire == 0) {
+                    break;
+                }
+
+            }
+        } catch (ArrayIndexOutOfBoundsException ex) {
+
+            System.err.println("Indice minore di 0");
+        } catch (NumberFormatException ex) {
+            System.err.println("Devi inserire un numero");
+        }
+
+
+    }
+}
